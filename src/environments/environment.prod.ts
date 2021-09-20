ccc06c0abae4f795fcbc3e714f53737897822b6d
Arpit <arpited7@gmail.com>
2021-09-20 18:19:41 +0545

    dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://toursandmore.herokuapp.com/api',
+  imageUrl: 'https://toursandmore.herokuapp.com/uploads'
 };
